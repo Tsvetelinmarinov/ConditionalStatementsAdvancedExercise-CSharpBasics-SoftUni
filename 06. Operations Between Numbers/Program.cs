87158a360888c6346205d4c88426eba62eaaae73
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-16 21:47:30 +0200

    Задача 5

---
+++ 06. Operations Between Numbers/Program.cs
@@ -0,0 +1,75 @@
+﻿/**
+ * 06. Operations Between Numbers
+ */
+
+namespace _06._Operations_Between_Numbers
+{
+
+    class Program
+    {
+
+        static void Main(string[] arguments)
+        {
+
+            //•	N1 – цяло число в интервала[0...1 000 000]
+            //•	N2 – цяло число в интервала[0...1 000 000]
+            //•	Оператор – един символ измежду: „+“, „-“, „*“, „/“, „%“
+
+            int leftNumber = int.Parse(Console.ReadLine());
+            int rigthNumber = int.Parse(Console.ReadLine());
+            char Operator = char.Parse(Console.ReadLine());
+
+            string status = "";
+            string output = "";
+
+            if (rigthNumber == 0 && (Operator == '/' || Operator == '%'))
+            {
+                output = $"Cannot divide {leftNumber} by zero";
+                Console.WriteLine(output);
+                Environment.Exit(0);
+            }
+
+            double result = Operator switch
+            {
+                '+' => leftNumber + rigthNumber,
+                '-' => leftNumber - rigthNumber,
+                '*' => leftNumber * rigthNumber,
+                '/' => leftNumber / (rigthNumber * 1.0),
+                '%' => leftNumber % rigthNumber,
+                _ => 0
+            };
+
+            if (result % 2 == 0)
+            {
+                status = "even";
+            }
+            else
+            {
+                status = "odd";
+            }
+
+            //Да се отпечата на конзолата един ред:
+            //•	Ако операцията е деление:
+            //o   "{N1} / {N2} = {резултат}" – резултатът е форматиран до вторият знак след дес.запетая
+            //•	Ако операцията е модулно деление:
+            //o   "{N1} % {N2} = {остатък}"
+            //•	В случай на деление с 0(нула): 
+            //o   "Cannot divide {N1} by zero"
+
+            //10 + 12 = 22 - even
+
+            output = Operator switch
+            {
+                '+' or '-' or '*' => $"{leftNumber} {Operator} {rigthNumber} = {result} - {status}",   
+                '/' => $"{leftNumber} {Operator} {rigthNumber} = {result:F2}",
+                '%' => $"{leftNumber} {Operator} {rigthNumber} = {result}",
+                _ => ""
+            };
+
+            Console.WriteLine(output);
+
+        }
+
+    }
+
+}
---
+++ 5. Пътешествие/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+/**
+ * 5.	Пътешествие
+ */
+
+namespace _5._Пътешествие
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //•	Първи ред – Бюджет, реално число в интервала [10.00...5000.00].
+            //•	Втори ред –  Един от двата възможни сезона: „summer” или “winter”
+
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string destination = "";
+            double tripPrice = 0;
+
+            //Бюджета определя дестинацията, а сезона определя колко от бюджета ще изхарчи.
+            //Ако е лято ще почива на къмпинг, а зимата в хотел. Ако е в Европа, независимо от
+            //сезона ще почива в хотел. Всеки къмпинг или хотел, според дестинацията, има собствена
+            //цена която отговаря на даден процент от бюджета:
+
+            string buildingType = season switch
+            {
+                "summer" => "Camp",
+                "winter" => "Hotel",
+                _ => ""
+            };
+
+            if (budget > 1000)
+            {
+                buildingType = "Hotel";
+            }
+
+            if (budget > 0 && budget <= 100)
+            {
+                destination = "Bulgaria";
+                tripPrice = season switch
+                {
+                    "summer" => budget * 0.3,
+                    "winter" => budget * 0.7,
+                    _ => 0
+                };
+            }
+            else if (budget > 100 && budget <= 1000)
+            {
+                destination = "Balkans";
+                tripPrice = season switch
+                {
+                    "summer" => budget * 0.4,
+                    "winter" => budget * 0.8,
+                    _ => 0
+                };
+            }
+            else
+            {
+                destination = "Europe";
+                tripPrice = budget * 0.9;
+            }
+
+            //•	Първи ред – „Somewhere in [дестинация]“ измежду “Bulgaria", "Balkans” и ”Europe”
+            //•	Втори ред – “{Вид почивка} - {Похарчена сума}“
+            //o	Почивката може да е между „Camp” и „Hotel”
+            //o	Сумата трябва да е закръглена с точност до вторият знак след запетаята
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{buildingType} - {tripPrice:F2}");
+
+        }
+
+    }
+
+}
